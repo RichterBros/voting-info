02fe72bdb4ebc253f6fc4b8eee073d9bd34864e5
Kevin Davis <thekidnamedkd@gmail.com>
2020-06-04 11:02:48 -0700

    stabilize age function to display some div targets.
    Co-authored-by: Ben Russell <benjaminrussell36@gmail.com>
    Co-authored-by: Erich Richter <erich.richter@gmail.com>

--- js/styles.js
+++ js/styles.js
@@ -1,23 +1,18 @@
 $(document).ready(function() {
-  $("#age-btn").submit(function(event){
+  $("form#age-info").submit(function(event){
     event.preventDefault();
-    const age = parseInt($("#age").val());
-  });
-votingAge();
+    const age =  parseInt($("#age").val());
 
+    if(age < 16) {
+    $("#minor").show();
+    }
+    else if(age >= 16 || age < 18) {
+    $("#prereg").show();
+    }
+    else if(age >= 18) {
+    $("#adult").show();
+    } 
+  });
 });
 
 
-function votingAge() {
-
-if(age < 16) {
-  $("#minor").show();
-}
-else if(age >= 16 || < 18) {
-  $("#prereg").show();
-}
-else(age >= 18) {
-  $("#adult").show();
-}
-
-};
