158b0ba7a520a9965cb329800db6ea2dc429b10a
Kevin Davis <thekidnamedkd@gmail.com>
2020-06-04 11:34:22 -0700

    age inputs show/hide div targets.
    Co-authored-by: Ben Russell <benjaminrussell36@gmail.com>
    Co-authored-by: Erich Richter <erich.richter@gmail.com>

--- js/styles.js
+++ js/styles.js
@@ -2,17 +2,23 @@ $(document).ready(function() {
   $("form#age-info").submit(function(event){
     event.preventDefault();
     const age =  parseInt($("#age").val());
-
+    
     if(age < 16) {
     $("#minor").show();
+    $("#prereg").hide();
+    $("#adult").hide();
     }
-    else if(age >= 16 || age < 18) {
+    else if(age >= 16 && age < 18) {
     $("#prereg").show();
+    $("#minor").hide();
+    $("#adult").hide();
     }
-    else if(age >= 18) {
+    else {
     $("#adult").show();
+    $("#prereg").hide();
+    $("#minor").hide();
     } 
-  });
+    });
 });
 
 
